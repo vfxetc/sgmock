d93cf264afd4b4eac1a9aabf7415bf3fa3e1825b
Mike Boers <mboers@keypics.com>
2012-09-12 09:41:49 -0700

    Only add unittest 2.7 extensions if < 2.7

--- sgmock/unittest.py
+++ sgmock/unittest.py
@@ -1,27 +1,12 @@
 from __future__ import absolute_import
 
 from unittest import TestCase as BaseTestCase
+import sys
 
 
 class TestCase(BaseTestCase):
     
-    def assertIn(self, key, collection):
-        if key not in collection:
-            self.fail('%r not in %r' % (key, collection))
-    
-    def assertIsInstance(self, instance, types):
-        if not isinstance(instance, types):
-            self.fail('not an instance of %r; %r' % (types, instance))
-    
-    def assertIs(self, a, b):
-        if a is not b:
-            self.fail('%r at 0x%x is not %r at 0x%x; %r is not %r' % (type(a), id(a), type(b), id(b), a, b))
-    
-    def assertIsNot(self, a, b):
-        if a is b:
-            self.fail('both are %r at 0x%x; %r' % (type(a), id(a), a))
-    
-    def assertSameEntity(self, a, b):
+    def assertSameEntity(self, a, b, msg=None):
         errors = []
         
         for loc, x in (('1st', a), ('2nd', b)):
@@ -40,5 +25,40 @@ class TestCase(BaseTestCase):
                     errors.append('ids do not match; %r != %r' % (a['id'], b['id']))
         
         if errors:
-            self.fail('; '.join(errors))
+            self.fail(msg or '; '.join(errors))
             return
+    
+    # Add some of the unittest methods that we love from 2.7.
+    if sys.version_info < (2, 7):
+    
+        def assertIs(self, a, b, msg=None):
+            if a is not b:
+                self.fail(msg or '%r at 0x%x is not %r at 0x%x; %r is not %r' % (type(a), id(a), type(b), id(b), a, b))
+    
+        def assertIsNot(self, a, b, msg=None):
+            if a is b:
+                self.fail(msg or 'both are %r at 0x%x; %r' % (type(a), id(a), a))
+        
+        def assertIsNone(self, x, msg=None):
+            if x is not None:
+                self.fail(msg or 'is not None; %r' % x)
+        
+        def assertIsNotNone(self, x, msg=None):
+            if x is None:
+                self.fail(msg or 'is None; %r' % x)
+        
+        def assertIn(self, a, b, msg=None):
+            if a not in b:
+                self.fail(msg or '%r not in %r' % (a, b))
+        
+        def assertNotIn(self, a, b, msg=None):
+            if a in b:
+                self.fail(msg or '%r in %r' % (a, b))
+        
+        def assertIsInstance(self, instance, types, msg=None):
+            if not isinstance(instance, types):
+                self.fail(msg or 'not an instance of %r; %r' % (types, instance))
+        
+        def assertNotIsInstance(self, instance, types, msg=None):
+            if isinstance(instance, types):
+                self.fail(msg or 'is an instance of %r; %r' % (types, instance))
